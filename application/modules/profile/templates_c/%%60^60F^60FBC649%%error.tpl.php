1fe8b6e91c387748b7d794f9cb9477150213aabd
lds.svn <lds2012@gmail.com>
2009-12-18 11:21:05 +0000

    ignore templates_c

--- application/modules/profile/templates_c/%%60^60F^60FBC649%%error.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2009-12-18 16:13:24
-         compiled from /home/svn/0019/trunk/application/templates/error.tpl */ ?>
-<span class="from_error">
-  <?php if ($this->_tpl_vars['error']): ?>
-	 <- <?php echo $this->_tpl_vars['error']; ?>
-
-  <?php endif; ?>
-</span>
