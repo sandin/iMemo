b0d05b374a9b60f79241291e9891e23f2c70e5dc
lds.svn <lds2012@gmail.com>
2010-01-01 02:01:42 +0000

    update and config to xml

--- public/index.php
+++ public/index.php
@@ -31,7 +31,8 @@ require_once 'Zend/Application.php';
 // Create application, bootstrap, and run
 $application = new Zend_Application(
     APPLICATION_ENV, 
-    APPLICATION_PATH . '/configs/application.ini'
+    //APPLICATION_PATH . '/configs/application.ini'
+    APPLICATION_PATH . '/configs/application.xml'
 );
 
 $application->bootstrap()
